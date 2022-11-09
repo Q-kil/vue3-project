f20f4ba19e14ab764bbf70ee611bc386e696f924
Q-kil <kaifawebb@gmail.com>
2022-11-10 00:21:33 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div>{{count}}</div>
+  <test></test>
 </template>
 
 <script>
+import test from './components/test.vue'
 export default {
+  components: {
+    test
+  },
   data(){
     return {
       count: 1
---
+++ src/components/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>test</h1>
+  <p>{{dummy}}</p>
+</template>
+
+<script setup>
+import { computed, effect, reactive, ref } from "vue";
+// const counter = ref({num1:1, num2:2})
+const counter = reactive({num1:1, num2:2})
+
+// let dummy = counter.num1 + counter.num2
+// let dummy = computed(()=>{
+//   return counter.num1 + counter.num2
+// })
+let dummy
+effect(()=>{
+  dummy = counter.num1 + counter.num2
+  console.log(dummy);
+})
+setInterval(()=>{
+  counter.num1++
+}, 1000)
+</script>
+
+<style>
+
+</style>
