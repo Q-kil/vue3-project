5ce07fbb1a11b3ab4253c76e3eec8466dacb3dd9
Q-kil <kaifawebb@gmail.com>
2022-11-14 00:00:49 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <div>{{count}}</div>
-  <test></test>
+  <a href="#/">首页 hello</a> 
+  <a href="#/about" style="margin-left:10px">about</a>
+  <!-- <div>{{count}}</div> -->
+  <!-- <test></test> -->
+  <component :is="currentView" />
 </template>
 
-<script>
-import test from './components/test.vue'
-export default {
-  components: {
-    test
-  },
-  data(){
-    return {
-      count: 1
-    }
-  }
+<script setup>
+import { computed, ref } from 'vue'
+import About from './components/About.vue'
+import HelloWorld from './components/HelloWorld.vue'
+const count = ref(1)
+const routes={
+  '/': HelloWorld,
+  '/about':About
 }
+
+const currentPath = ref(window.location.hash)
+console.log('current:', currentPath);
+window.addEventListener('hashchange',()=>{
+  currentPath.value=window.location.hash
+  console.log('hashchange', currentPath);
+})
+const currentView=computed(()=>{
+  console.log('currentPath.value.slice(1)', currentPath.value);
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
 </script>
 
 <style>
-
 </style>
