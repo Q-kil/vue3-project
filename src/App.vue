7039fa90fdb69a6a50f678eac5aceb2c2ebc0ea3
Q-kil <kaifawebb@gmail.com>
2023-05-16 23:40:09 +0800

    child

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,16 @@
   <a href="#/about" style="margin-left:10px">about</a>
   <!-- <div>{{count}}</div> -->
   <!-- <test></test> -->
-  <component :is="currentView" />
+  <!-- <component :is="currentView" /> -->
+  <!-- <About :id="id"></About> -->
+  <Child :id="id"></Child>
 </template>
 
 <script setup>
 import { computed, ref } from 'vue'
 import About from './components/About.vue'
 import HelloWorld from './components/HelloWorld.vue'
+import Child from './components/Child.vue'
 const count = ref(1)
 const routes={
   '/': HelloWorld,
@@ -18,6 +21,8 @@ const routes={
 
 const currentPath = ref(window.location.hash)
 console.log('current:', currentPath);
+const id = ref('888');
+
 window.addEventListener('hashchange',()=>{
   currentPath.value=window.location.hash
   console.log('hashchange', currentPath);
@@ -26,6 +31,9 @@ const currentView=computed(()=>{
   console.log('currentPath.value.slice(1)', currentPath.value);
   return routes[currentPath.value.slice(1) || '/'] || NotFound
 })
+setTimeout(() => {
+  id.value = 'Hello World';
+}, 1000);
 </script>
 
 <style>
