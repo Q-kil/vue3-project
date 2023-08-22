9e6fd0b08cc629abfc2bfb1823bc2e4e1a90da4c
Q-kil <kaifawebb@gmail.com>
2023-08-22 18:45:00 +0800

    ref

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,37 @@
     <h1>Home</h1>
     <button @click="increment">Count is: {{ count }}</button>
     <Empty></Empty>
+
+    <div>
+      <p>Object</p>
+      <p>obj: {{ obj }}</p>
+      <p>obj.nested.foo: {{ obj.nested.foo }}</p>
+      <p>obj.array: {{ obj.array }}</p>
+      <button @click="mutateDeeply">change</button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue' 
+import { nextTick, ref } from 'vue' 
 const count = ref(0)
-console.log('count', count)
+console.log('count', count, JSON.stringify(count))
 console.log('count', count.value)
 function increment() {
   count.value++
+}
+
+const obj = ref({
+  nested: {
+    foo: 1
+  },
+  array: [1, 2, 3]
+})
+async function mutateDeeply() {
+  obj.value.nested.foo++
+  obj.value.array.push(4)
+  await nextTick()
+  console.log('dom 更新了');
 }
 </script>
 
